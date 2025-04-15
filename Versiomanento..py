3cb39bec60800aba251a03a2d3aba461f7878e01
Arzioti <toony.junior@gmail.com>
2025-04-15 20:19:25 -0300

    detalhar

--- Versiomanento..py
+++ Versiomanento..py
@@ -2,13 +2,12 @@
 
 print("Git Hub é a salvação!!!")
 
-
-# #primeiro cria repo remoto
-# faz o git clone local 
-# faz as modificação
-# git add
-# git commit
-# git push origin main
+# #primeiro cria repo remoto (Criar diretório no github criar readme - MIT, license, gitignore - python)
+# faz o git clone local (Criar um diretorio local e fazer um clone)
+# faz as modificação (Realizar as tarefas que deseja no codigo)
+# git add (Adiciona as modificações necessárias)
+# git commit (Comenta as modificações)
+# git push origin main (Envia o arquivo modificado para o github)
 
 
 # !!Caminhofeliz"""
