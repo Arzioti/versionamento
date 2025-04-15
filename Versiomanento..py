70489d4e3dc0108834355502f76e7afc6038d4d5
Arzioti <toony.junior@gmail.com>
2025-04-15 20:13:49 -0300

    caminho

--- Versiomanento..py
+++ Versiomanento..py
@@ -1,3 +1,14 @@
 # Usando o github para armazenar arquivos
 
 print("Git Hub é a salvação!!!")
+
+
+# #primeiro cria repo remoto
+# faz o git clone local 
+# faz as modificação
+# git add
+# git commit
+# git push origin main
+
+
+# !!Caminhofeliz"""
